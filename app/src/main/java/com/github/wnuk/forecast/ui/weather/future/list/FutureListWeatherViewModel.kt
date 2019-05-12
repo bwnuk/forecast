aba67c88d6b6cc90f5d1a2df6b9dcbb65083a259
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2019-05-12 20:42:49 +0200

    Begin of the navigation.

---
+++ app/src/main/java/com/github/wnuk/forecast/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.wnuk.forecast.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel;
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
